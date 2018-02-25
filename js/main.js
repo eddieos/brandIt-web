2ab375b9098c286ae202ab925f654127b117bb88
Ekhoragbon Endurance <eekhoragbon@gmail.com>
2018-02-25 23:52:51 +0100

    feat: read more link introduced

--- js/main.js
+++ js/main.js
@@ -46,14 +46,11 @@ jQuery(document).ready(function($){
             }
         }
     }
-});
-
-$(document).ready(function() {
-    $('#fullpage').fullpage({
+    $('#about').fullpage({
         //Navigation
         menu: '#menu',
         lockAnchors: false,
-        anchors:['About', 'Who We Are'],
+        anchors:['About', 'Who We Are', 'Clients'],
         navigation: true,
         navigationPosition: 'right',
         navigationTooltips: ['About', 'Who We Are'],
@@ -80,7 +77,80 @@ $(document).ready(function() {
         dragAndMove: false,
         offsetSections: false,
         resetSliders: false,
-        fadingEffect: false,
+        fadingEffect: true,
+        normalScrollElements: '#element1, .element2',
+        scrollOverflow: false,
+        scrollOverflowReset: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: true,
+        sectionsColor : ['#ccc', '#fff'],
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+        parallax: false,
+        parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        lazyLoading: true,
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterResponsive: function(isResponsive){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
+    $('#about').fullpage({
+        //Navigation
+        menu: '#menu',
+        lockAnchors: false,
+        anchors:['About', 'Who We Are', 'Clients'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['About', 'Who We Are'],
+        showActiveTooltip: false,
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: false,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: false,
+        dragAndMove: false,
+        offsetSections: false,
+        resetSliders: false,
+        fadingEffect: true,
         normalScrollElements: '#element1, .element2',
         scrollOverflow: false,
         scrollOverflowReset: false,
